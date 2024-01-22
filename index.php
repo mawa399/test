096a3dfa2baf76a2bb75b4fab615e3cc2ea4cb58
Diaw <97046954+mawa399@users.noreply.github.com>
2024-01-22 20:57:19 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Eligibilité des immeubles à la fibre optique</title>
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./css/style.css"/>
+    <link rel="stylesheet" href="https://unpkg.com/esri-leaflet-geocoder/dist/esri-leaflet-geocoder.css" />
+      <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://unpkg.com/esri-leaflet"></script>
+    <script src="https://unpkg.com/esri-leaflet-geocoder"></script>
+    <script src="Plugins/leaflet.browser.print-master/dist/leaflet.browser.print.min.js"></script>
+    <!--    <script type="text/javascript" language="javascript" src="js/leaflet.markercluster.js"></script> -->
+
+    <script src="./js/script.js"></script>
+</head>
+<body>
+     <nav>
+        <img src="./images/logo.png" width="20%" class="logo">
+        <h1>Eligibilité des immeubles à la fibre optique</h1>
+        <a href="apropos.html" class="about-link">
+            <img src="./images/apropos.png" alt="À propos" class="about-icon">
+            À propos
+        </a>
+
+    </nav>
+    <div class="content">
+        
+        <div id="map"></div>
+        
+            
+            </div>
+        
+    
+    <div id="geocoder-container"></div>
+
+     <div class="bottom-section">
+        <b><p class="network-text">Nos Réseaux</p></b>
+        <div class="social-media">
+            <a href="https://www.facebook.com/HeraultNumerique" target="_blank" class="social-icon">
+                <img src="./images/facebook.png" alt="Facebook">
+            </a>
+            <a href="https://twitter.com/i/flow/login?redirect_after_login=%2Fheraultinfos" target="_blank" class="social-icon">
+                <img src="./images/twitter.png" alt="Twitter">
+            </a>
+            <a href="https://www.instagram.com/departementherault" target="_blank" class="social-icon">
+                <img src="./images/instagram.png" alt="Instagram">
+            </a>
+            <a href="https://www.youtube.com/playlist?list=PL0RN4iB5EZDGEy-XGWIOuBhtR2hot_Tpc" target="_blank" class="social-icon">
+                <img src="./images/youtube.png" alt="YouTube">
+            </a>
+        </div>
+    </div>
+  
+</body>
+</html>
